b423338fbe6ed24c79ca9b80949eb5a53580e08a
Lucas Källberg <lucas.arne@hotmail.com>
2018-04-05 17:34:42 +0200

    Add project files.

---
+++ FunQuiz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FunQuiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
